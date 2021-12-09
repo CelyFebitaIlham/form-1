167f94cbc05fd0e06fa9982bf3126723743acdae
CELY FEBITA ILHAM <92569794+CelyFebitaIlham@users.noreply.github.com>
2021-12-09 08:53:00 +0700

    Add files via upload

---
+++ form1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main (){
+    //input  
+    
+    string nama;
+    string npm;
+    string alamat;
+    
+    cout<<"Masukan Nama Anda   = ";
+    getline(cin,nama);
+    cout<<"Masukan NPM Anda    = ";
+    cin>>npm;
+    cout<<"Masukan Alamat Anda = ";
+    cin>>alamat;
+    
+    //output
+    
+    cout<<" Nama saya adalah " <<nama<< " dengan npm " <<npm<< " dan beralamat di " <<alamat<<endl;
+    
+    
+   
+    
+    cout<<">//<"<<endl;
+    cout<<"Cely Febita Ilham";
+    
+   
+    
+    
+    return 0;
+}
+    
+    
